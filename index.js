ed003bcaf777bd4f86c660329387f4f64c42556b
prankland4 <thibaut.verbeke@hotmail.com>
2020-04-28 16:21:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ client.on("message", async message => {
  
     var command = messageArray[0];
 
-    if (command === `${prefix}hallo`){
+    if (command === `${prefix}hello`){
 
-        return message.reply("hallo")
+        return message.reply("hello")
 
     }
 
@@ -36,12 +36,13 @@ client.on("message", async message => {
 
         var embedHelp = new discord.MessageEmbed()
         .setColor("#3481ed")
-        .setTitle("dit zijn alle comands van de bot")
-        .setDescription(`**voor de bezoekers**
-        ** ${prefix}hallo:** laat de bot hallo terug sturen
-        ** ${prefix}help:** heeft alle comands aan
-        ** voor de staff leden die mensen kunnen kicken**
-        ** ${prefix}kick:** wil je iemand kicken om dat hij de regels bv. niet naleefd (zitten nog fouten in)`);
+        .setTitle("these are the comands of the bot")
+        .setDescription(`**for the visitors**
+        ** ${prefix}hello:** have the bot send hello back
+        ** ${prefix}invite:** show the invite link
+        ** ${prefix}help:** has all the comands on
+        **for the staff members who can kick people**
+        ** ${prefix}kick:** do you want to kick someone because he does not comply with the rules, for example (are still errors)`);
 
         return message.reply(embedHelp)
 
@@ -51,48 +52,48 @@ client.on("message", async message => {
        
         var args = message.content.slice(prefix.length).split(/ +/);
  
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry jij bent geen staff lid die dit cmd mag uitvoeren");
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry you are not a staff member who is allowed to run this cmd");
  
         if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("geen perms");
  
-        if(!args[1]) return message.reply("sry je hebt geen gebruiker opgegeven pls doe dat!!!");
+        if(!args[1]) return message.reply("sry you have no user specified pls do that !!!");
  
-        if(!args[2]) return message.reply("sry je hebt geen redennen opgegeven pls doe dat!!!");
+        if(!args[2]) return message.reply("sry you have no reasons given pls do that !!!");
  
         var kickUser = message.guild.member(message.mentions.users.first()  || message.guild.members.get(args[1]));
  
         var reasen = args.slice(2).join(" ");
  
-        if(!kickUser) return message.reply("gebruiker niet gevonden");
+        if(!kickUser) return message.reply("user not found");
  
         var embedPromt = new discord.MessageEmbed()
             .setColor("GREEN")
-            .setTitle("gelieve binnen 30 seconden te reageren")
+            .setTitle("please respond within 30 seconds")
             .setDescription(`wil je ${kickUser} kicken?`);
  
         var embedKicked = new discord.MessageEmbed()
             .setColor("RED")
             .setFooter(message.member.displayName)
             .setTimestamp()
-            .setDescription(`**gekickt: ** ${kickUser}(${kickUser.id})
-            **gekickt door: ** ${message.author}
-            **redennen: ** ${reasen}`);
+            .setDescription(`**kicked: ** ${kickUser}(${kickUser.id})
+            **kicked by: ** ${message.author}
+            **reasons: ** ${reasen}`);
  
         message.channel.send(embedPromt).then(async msg =>{
  
         message.channel.awaitMessages(m => m.author.id === message.author.id, {max: 1, time: 30000}).then(collected =>{
  
-            if(collected.first().content.toLocaleLowerCase() == `ja`) {
+            if(collected.first().content.toLocaleLowerCase() == `yes`) {
  
                 kickUser.kick(reason).catch(err => {
  
-                    if(err) return message.reply("er is iets mis gelopen")
+                    if(err) return message.reply("something went wrong")
  
                 });
  
             }else{
  
-                message.reply("canceld");
+                message.reply("canceled");
  
             }
         });
