f1b0569a10c1caabb1a975e35cf438e314b8c4f3
prankland3 <64473540+prankland3@users.noreply.github.com>
2021-01-18 08:59:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -227,4 +227,41 @@ async function promptMessage(message, author, time, reactions) {
     // Dan kunnen we bericht terug sturen met dat icoontje dat is aangeduid.
     return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
 }
+
+if (command === `${prefix}hello`){
+
+   // !clear aantal
+   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming");
+ 
+   if (!args[0]) return message.reply("Geef een aantal op dat je weg wilt halen");
+
+   if (Number.isInteger(parseInt(args[0]))) {
+
+       var aantal = parseInt(args[0]) + 1;
+
+       message.channel.bulkDelete(aantal).then(() => { 
+
+           if (args[0] == 0) {
+
+               message.reply(`Ben je loemp ik kan toch niet 0 berichten verwijderen?`).then(msg => msg.delete({timeout: 3000}));
+           
+           } else if (args[0] == 1) {
+           
+               message.reply(`Ik heb 1 bericht verwijderd.`).then(msg => msg.delete({timeout: 3000}));
+           
+           } else {
+           
+               message.reply(`Ik heb ${args[0]} berichten verwijderd.`).then(msg => msg.delete({timeout: 3000}));
+           
+           }
+
+       });
+
+   } else {
+       return message.reply("Geef een getal op.");
+   }
+
+
+}
+
 });
